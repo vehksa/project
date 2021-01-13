a0a7b77eb3ea1746cefaf4a9726f97f09dbcbcf3
vehksa <75016132+vehksa@users.noreply.github.com>
2021-01-13 10:22:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
+document.getElementById("run").onclick = function hlight(e) {
+    let field1 = document.getElementById("field1");
+    let field2 = document.getElementById("field2");
 
+    let field1Text = field1.innerHTML;
+    let field2Text = field2.innerHTML;
 
-document.getElementById("run").onclick = function hlight (e){ 
-  var field1 = document.getElementById("field1").value;
-  var field2 = document.getElementById("field2").value,
-      text = '';
-  field1.split('').forEach(function(val, i){
-    if (val != field2.charAt(i))
-    text += "<span class='highlight'>" + val+ "</span>";  
-    else
-    text += val; 
-  });
-  
-  document.getElementById("field1").value = text;
+    let max_value = Math.max(field1Text.length, field2Text.length);
+
+    for (let i = max_value; i >= 0; i--) {
+        let char1 = field1Text.charAt(i);
+        let char2 = field2Text.charAt(i);
+
+        if (char1 !== char2) {
+            highlight(field1, i);
+            highlight(field2, i);
+        }
+    }
 }
 
-document.getElementById("clearbutton").onclick = function(e) { 
-    document.getElementById("field1").value = "";
-    document.getElementById("field2").value = "";}
+function highlight(element, index) {
+    var inputText = element;
+    var innerHTML = inputText.innerHTML;
+    if (index >= 0) {
+        innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + 1) + "</span>" + innerHTML.substring(index + 1);
+        inputText.innerHTML = innerHTML;
+    }
+}
+
+document.getElementById("clearbutton").onclick = function (e) {
+    document.getElementById("field1").innerHTML = "";
+    document.getElementById("field2").innerHTML = "";
+}
